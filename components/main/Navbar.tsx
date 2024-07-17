3eab32f556593bc6207973725b8163e491d5c69f
VaibhavMathur03 <mathurvaibhav010@gmail.com>
2024-07-17 23:49:58 +0530

    LCP correction and resume update

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -12,21 +12,21 @@ const Navbar = () => {
     <div className="w-full fixed top-0 shadow-lg shadow-[#2A0E61]/50 bg-[#03001417] backdrop-blur-md z-50">
       
       <div className="w-full flex flex-row items-center justify-between m-auto px-4 py-2 md:px-10 md:py-4">
-      <button className="ml-4 text-white md:hidden" onClick={() => setIsMenuOpen(!isMenuOpen)}>
+      <button name="menu" className="ml-4 text-white md:hidden" onClick={() => setIsMenuOpen(!isMenuOpen)}>
             {isMenuOpen ? <HiX size={24} /> : <HiMenu size={24} />}
           </button>
         <a href="#about-me" className="flex flex-row items-center" aria-label="aboutMe">
           <span className="font-bold text-gray-300">Portfolio</span>
         </a>
 
-        <div className="hidden w-1/2 md:flex items-center justify-between w-auto border border-[#b9a3fa] bg-[#0300145e] px-6 py-2 rounded-full text-gray-200">
-          <Link href="/" className="mx-2 hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r from-purple-200 via-blue-100 to-indigo-200 hover:font-semibold" aria-label="aboutMe">
+        <div className="hidden w-1/2 md:flex items-center justify-between border border-[#b9a3fa] bg-[#0300145e] px-6 py-2 rounded-full text-gray-200">
+          <Link prefetch={false} href="/" className="mx-2 hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r from-purple-200 via-blue-100 to-indigo-200 hover:font-semibold" aria-label="aboutMe">
             About me
           </Link>
-          <Link href="/skills" className="mx-2 hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r from-purple-200 via-blue-100 to-indigo-200 hover:font-semibold" aria-label="Skills">
+          <Link prefetch={false} href="/skills" className="mx-2 hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r from-purple-200 via-blue-100 to-indigo-200 hover:font-semibold" aria-label="Skills">
             Skills
           </Link>
-          <Link href="/projects" className="mx-2 hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r from-purple-200 via-blue-100 to-indigo-200 hover:font-semibold" aria-label="Projects">
+          <Link prefetch={false} href="/projects" className="mx-2 hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r from-purple-200 via-blue-100 to-indigo-200 hover:font-semibold" aria-label="Projects">
             Projects
           </Link>
         </div>
--- components/main/Profile.tsx
+++ components/main/Profile.tsx
@@ -3,6 +3,7 @@ import { RxGithubLogo } from "react-icons/rx";
 
 import { SiGmail } from "react-icons/si";
 import { FaLinkedin } from "react-icons/fa";
+import Link from "next/link";
 
 const Profile = () => {
   return (
@@ -11,35 +12,42 @@ const Profile = () => {
       className="flex items-center justify-center min-h-screen bg-black text-white overflow-hidden"
     >
       <div className="text-center z-20 ">
-        <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold opacity-100 brightness-150">
-          Hi, I&apos;m 
-          <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 via-blue-500 to-indigo-600">
-          &nbsp;Vaibhav
+      <h1 className="text-5xl font-mono md:text-6xl lg:text-7xl font-bold mb-6 animate-fade-in-down">
+      Hello; <br/> I&apos;m
+          <span className="text-transparent font-sans bg-clip-text bg-gradient-to-r from-purple-400 via-blue-500 to-indigo-600">
+            &nbsp;Vaibhav.
           </span>
         </h1>
-        <h2 className="text-3xl md:text-4xl lg:text-5xl font-bold mt-4">
-          I&apos;m a Developer & SaaS Enthusiast.
-        </h2>
-        <p className="text-white text-xl mt-4 max-w-xl mx-auto">
-          I&apos;m a B.Tech undergrad in Computer Science who loves diving into the
-          world of technology! With industry-level experience in full stack web
-          development, I enjoy creating sleek, efficient web applications from
-          scratch. But that&apos;s not all—I also have a passion for data science and
-          machine learning specially in the field of finance.
+
+        <p className="text-2xl font-serif tracking-wide md:text-3xl max-w-xl mb-8">
+          I&apos;m a software developer, specializing in{" "}
+          <span className="italic">Scalable Web Development</span>{" "}
+          and{" "}
+          <span className="italic">
+            Machine Learning in Finance
+          </span>
+          .
         </p>
+       
         <div className="mt-8 space-x-4">
-            <a href="https://drive.google.com/file/d/1U2et6QWx0MtH67rHL9u4Wkf1n9ovq4kE/view?usp=drive_link" className="relative inline-block text-lg group" aria-label="resume">
+            <a href="https://drive.google.com/drive/folders/1sucW3IVji84K4OPK9nlTlGznj13_lrNH?usp=drive_link" target="_blank" className="relative inline-block text-lg group" aria-label="resume">
               <span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
                 <span className="absolute inset-0 w-full h-full px-2 py-1 rounded-lg bg-gray-50"></span>
                 <span className="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease"></span>
-                <span className="relative">My Resume</span>
+                <span className="relative">Resume</span>
               </span>
-              <span
-                className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0"
-                data-rounded="rounded-lg"
-              ></span>
+              
             </a>
+            <Link prefetch={false} href={"/projects"} className="relative inline-block text-lg group" aria-label="resume">
+              <span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
+                <span className="absolute inset-0 w-full h-full px-2 py-1 rounded-lg bg-gray-50"></span>
+                <span className="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease"></span>
+                <span className="relative">Projects</span>
+              </span>
+              
+            </Link>
         </div>
+        
         <div className=" mt-5 h-auto flex  items-center justify-between">
           <a href="mailto:mathurvaibhav010@gmail.com" aria-label="mail">
             <p className="flex flex-row items-center my-[20px] cursor-pointer">
